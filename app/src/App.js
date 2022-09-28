c87528e3efb0b2f1da38c1aa0818463ae016568c
aadil davila <Aadilraza9763@gmail.com>
2022-09-28 16:29:31 +0530

    addi New Components and one File

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./Components/Home Page/Home";
 import NaveBar from "./Components/NaveBar/NaveBar";
 import Footer from "./Components/Footer/Footer";
+import Cards from "./Components/Cards/Cards";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       {/* <h1>This is app.js Components</h1> */}
       <NaveBar />
       <Home />
+      <Cards />
       <Footer />
     </div>
   );
---
+++ app/src/Components/Cards/Cards.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Cards.css";
+
+const Cards = () => {
+  return (
+    <div>
+      <marquee bgcolor="purple">
+        ExalTechs SoftWare Web Deyelopment Course 💻 , HTML5, CSS3, JavaScript,
+        React js, Redux, Typescript, React Bootstrap, Material ui
+      </marquee>
+    </div>
+  );
+};
+
+export default Cards;
